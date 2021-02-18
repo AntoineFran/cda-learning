b7872e6819f5473e0f536361c21a9a33531f83be
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-02-18 10:54:19 +0100

    Set the connection tests

--- src/test/java/com/afpa/learning/ConnectionTest.java
+++ src/test/java/com/afpa/learning/ConnectionTest.java
@@ -1,12 +1,29 @@
 package com.afpa.learning;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import com.afpa.learning.connection.DatabaseConnection;
 
 
 public class ConnectionTest {
 
+	private Connection connection;
+	
     @Test
-    public void DatabaseConnectionTest() {
-    
+    public void database_connection_null_test() {
+    	connection = null;
+    	assertEquals(null, connection);
     }
+    
+    @Test
+    public void database_connection_not_null_test() {
+    	connection = DatabaseConnection.getConnection();
+    	assertNotEquals(null, connection);
+    }
+    
 }
